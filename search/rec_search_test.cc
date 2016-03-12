aae6bfeb434a55b510d1354d49bd15cb8d6c8aee
Leif Schelin <schelinleif@gmail.com>
2016-03-12 11:02:27 +0100

    Added recursive version of binary search and fixed minor error

--- search/rec_search_test.cc
+++ search/rec_search_test.cc
@@ -5,45 +5,45 @@
 
 namespace search {
 
-TEST(binarysearch_empty) {
+TEST(rec_binarysearch_empty) {
   std::vector<int> arr = {};
-  ASSERT_EQ(-1, BinarySearch(arr, 0));
+  ASSERT_EQ(-1, RecursiveBinarySearch(arr, 0));
 }
 
-TEST(binarysearch_notfound) {
+TEST(rec_binarysearch_notfound) {
   std::vector<int> arr = {1, 2, 3, 7};
-  ASSERT_EQ(-1, BinarySearch(arr, 0));
-  ASSERT_EQ(-1, BinarySearch(arr, 5));
-  ASSERT_EQ(-1, BinarySearch(arr, 8));
+  ASSERT_EQ(-1, RecursiveBinarySearch(arr, 0));
+  ASSERT_EQ(-1, RecursiveBinarySearch(arr, 5));
+  ASSERT_EQ(-1, RecursiveBinarySearch(arr, 8));
 }
 
-TEST(binarysearch_found_even) {
+TEST(rec_binarysearch_found_even) {
   std::vector<int> arr = {1, 2, 3, 4};
-  ASSERT_EQ(0, BinarySearch(arr, 1));
-  ASSERT_EQ(2, BinarySearch(arr, 3));
-  ASSERT_EQ(3, BinarySearch(arr, 4));
+  ASSERT_EQ(0, RecursiveBinarySearch(arr, 1));
+  ASSERT_EQ(2, RecursiveBinarySearch(arr, 3));
+  ASSERT_EQ(3, RecursiveBinarySearch(arr, 4));
 }
 
-TEST(binarysearch_found_odd) {
+TEST(rec_binarysearch_found_odd) {
   std::vector<int> arr = {1, 3, 4};
-  ASSERT_EQ(0, BinarySearch(arr, 1));
-  ASSERT_EQ(1, BinarySearch(arr, 3));
-  ASSERT_EQ(2, BinarySearch(arr, 4));
+  ASSERT_EQ(0, RecursiveBinarySearch(arr, 1));
+  ASSERT_EQ(1, RecursiveBinarySearch(arr, 3));
+  ASSERT_EQ(2, RecursiveBinarySearch(arr, 4));
 }
 
-TEST(binarysearch_found_duplicates) {
+TEST(rec_binarysearch_found_duplicates) {
   std::vector<int> arr = {1, 3, 3, 3, 3, 4};
-  ASSERT_LT(0, BinarySearch(arr, 3));
+  ASSERT_LT(0, RecursiveBinarySearch(arr, 3));
 }
 
-TEST(binarysearch_greater) {
+TEST(rec_binarysearch_greater) {
   std::vector<int> arr = {4, 3, 1};
   {
-    int ix = BinarySearch<int, std::greater<int>>(arr, 1);
+    int ix = RecursiveBinarySearch<int, std::greater<int>>(arr, 1);
     ASSERT_EQ(2, ix);
   }
   {
-    int ix = BinarySearch<int, std::greater<int>>(arr, 0);
+    int ix = RecursiveBinarySearch<int, std::greater<int>>(arr, 0);
     ASSERT_EQ(-1, ix);
   }
 }
@@ -70,7 +70,7 @@ struct TestEq {
   }
 };
 
-TEST(binarysearch_obj) {
+TEST(rec_binarysearch_obj) {
   std::vector<TestValue> arr;
   arr.emplace_back(0, 0);
   arr.emplace_back(0, 1);
@@ -83,11 +83,11 @@ TEST(binarysearch_obj) {
   arr.emplace_back(1, 3);
   arr.emplace_back(2, 3);
   {
-    int ix = BinarySearch<TestValue, TestComp, TestEq>(arr, TestValue(0, 1));
+    int ix = RecursiveBinarySearch<TestValue, TestComp, TestEq>(arr, TestValue(0, 1));
     ASSERT_EQ(1, ix);
   }
   {
-    int ix = BinarySearch<TestValue, TestComp, TestEq>(arr, TestValue(1, 8));
+    int ix = RecursiveBinarySearch<TestValue, TestComp, TestEq>(arr, TestValue(1, 8));
     ASSERT_EQ(-1, ix);
   }
 }
--- search/search.h
+++ search/search.h
@@ -6,32 +6,54 @@ namespace search {
 template <class T, class Comp, class Eq>
 size_t BinarySearch(const std::vector<T>& arr, const T& value);
 
+template <class T, class Comp, class Eq>
+size_t RecursiveBinarySearch(const std::vector<T>& arr, const T& value);
+
 // Implementation --------------------
 
 template <class T, class Comp = std::less<T>, class Eq = std::equal_to<T>>
 size_t BinarySearch(const std::vector<T>& arr, const T& value) {
-  if (arr.empty()) return -1;
-
   Comp comparator;
   Eq equals;
   size_t low = 0;
   size_t end = arr.size();
-
-  if (comparator(value, arr[low])) { return -1;}
-  if (comparator(arr[end - 1], value)) { return -1;}
-
+ 
   while (low < end) {
     size_t mid = (low + end) / 2;
     if (equals(arr[mid], value)) {
       return mid;
-    } else if (low == mid) {
-      return -1;
     } else if (comparator(arr[mid], value)) {
-      low = mid;
+      low = mid + 1;
     } else {
       end = mid;
     }
   }
   return -1;
+}
+
+namespace internal {
+template <class T, class Comp, class Eq>
+size_t TailBinarySearch(const std::vector<T>& arr, const T& value,
+                             const Comp& comparator, const Eq& equals,
+                             size_t low, size_t end) {
+  if (low >= end) {
+    return -1;
+  }
+  size_t mid = (low + end) / 2;
+  if (equals(arr[mid], value)) {
+    return mid;
+  } else if (comparator(arr[mid], value)) {
+    return TailBinarySearch(arr, value, comparator, equals, mid + 1, end);
+  } else {
+    return TailBinarySearch(arr, value, comparator, equals, low, mid);
+  }
+}
+}  // namespace internal
+
+template <class T, class Comp = std::less<T>, class Eq = std::equal_to<T>>
+size_t RecursiveBinarySearch(const std::vector<T>& arr, const T& value) {
+  Comp comparator;
+  Eq equals;
+  return internal::TailBinarySearch(arr, value, comparator, equals, 0, arr.size());
 }
 }
